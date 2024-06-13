15458df96a3b4993262121a7170022e20bb2102e
loukosrotten <70531641+loukosrotten@users.noreply.github.com>
2024-06-13 12:14:50 +0300

    Add files via upload

---
+++ convert_to_bed.R
@@ -0,0 +1,16 @@
+convert_to_bed <-function(DFD_path, bed_path)
+{
+  for(i in seq(2,107,1))
+  {
+    filename = paste(DFD_path,i,"_tot.txt",sep="")
+    data = read.delim(filename, sep=" ")
+    #change columns first coords then gene start end
+    data_tmp = data.frame(data[,c(1,4,5)],stringsAsFactors = FALSE)
+    data_tmp$chromosome = paste("chr",data_tmp$chromosome,sep = "")
+    colnames(data_tmp) <- c("chrom","chromStart","chromEnd")
+    filenamew = paste0(bed_path,"DFDs_",i,".bed")
+    write.table(data_tmp,filenamew,col.names = F, quote = F)
+  }
+}
+
+convert_to_bed("/home/loukos/Desktop/encode/DFDs2/DFDs/total/", "/home/loukos/Desktop/")
