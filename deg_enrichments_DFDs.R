a02a8fd7aa3d719c178062d4b0ce8ecde7983c09
loukosrotten <70531641+loukosrotten@users.noreply.github.com>
2024-06-17 11:47:05 +0300

    Add files via upload

---
+++ deg_enrichments_DFDs.R
@@ -0,0 +1,20 @@
+deg_enrichments_DFDs <- function(DEG_DFD_Enrichment_path, categories_index)
+{
+  ########Do DEG Enrichment in DFDs##########
+  DEG_DFD_Enrichment <-read.delim(DEG_DFD_Enrichment_path, header = F, sep=" ")
+  data_categories <-read.delim(categories_index, sep="\t", header = F) 
+  type_of_tissue = data_categories
+  ##try with all overlap
+  DEG_DFD_Enrichment_df = as.data.frame(cbind(type_of_tissue$V1, DEG_DFD_Enrichment))
+  colnames(DEG_DFD_Enrichment_df)[1] <- "type_of_tissue"
+  krk = kruskal.test(V10 ~ type_of_tissue, DEG_DFD_Enrichment_df)
+  a = krk$p.value
+  ##try only with significant overlap
+  DEG_DFD_Enrichment_df = DEG_DFD_Enrichment_df[DEG_DFD_Enrichment_df$V12 < 0.05,]
+  krk = kruskal.test(V10 ~ type_of_tissue, DEG_DFD_Enrichment_df)
+  b = krk$p.value
+  output <- list(all_enrichments_pval = a, significant_enrichments_pval = b)
+  return(output) 
+}
+
+enrichment_pval <- deg_enrichments_DFDs("/home/loukos/Desktop/encode/DFDs2/DFDs/total2/results_sed2.txt", "/home/loukos/Desktop/phd/major_cell_type.txt")
