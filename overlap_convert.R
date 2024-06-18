2d75907e081039d996c63023d43febad5bff1fd8
loukosrotten <70531641+loukosrotten@users.noreply.github.com>
2024-06-18 12:46:20 +0300

    Update overlap_convert.R

--- overlap_convert.R
+++ overlap_convert.R
@@ -1,15 +1,4 @@
-dfds_overlap = "/home/loukos/Desktop/results_DFDS.txt"
-dfds_overlap2 = "/home/loukos/Desktop/results_DFDS2.txt"
-command = paste0("sed '/^File/!d' ", dfds_overlap, " > ", dfds_overlap2)
-system(command, intern = FALSE)
-#filename = paste(dfds_overlap,i,"_tot.txt",sep="")
-data_overlap = read.delim(dfds_overlap2, sep=" ", header = F)
-data_overlap$V10
-#data_overlap$V12
-for(i in seq(1,length(data_overlap$V10),1))
-{
-  
-}
+
 overlap_convert <- function(dfds_overlap_input, dfds_overlap_output)
 {
   command = paste0("sed '/^File/!d' ", dfds_overlap_input, " > ", dfds_overlap_output)
